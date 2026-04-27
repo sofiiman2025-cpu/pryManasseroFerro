46b1ae306f856d481cd81522670e7f61021f4811
Sofía <sofiiman2025@gmail.com>
2026-04-27 10:06:00 -0300

    Agregar archivos de proyecto.

---
+++ pryManasseroFerro/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace pryManasseroFerro
+{
+    partial class frmOrganizacionferro
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmOrganizacionferro));
+            this.lblBoletos = new System.Windows.Forms.Label();
+            this.lblDistancia = new System.Windows.Forms.Label();
+            this.lblDiasEstadia = new System.Windows.Forms.Label();
+            this.txtDistancia = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.numDias = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numDias)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblBoletos
+            // 
+            this.lblBoletos.AutoSize = true;
+            this.lblBoletos.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoletos.Location = new System.Drawing.Point(51, 9);
+            this.lblBoletos.Name = "lblBoletos";
+            this.lblBoletos.Size = new System.Drawing.Size(59, 19);
+            this.lblBoletos.TabIndex = 0;
+            this.lblBoletos.Text = "Boletos";
+            // 
+            // lblDistancia
+            // 
+            this.lblDistancia.AutoSize = true;
+            this.lblDistancia.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDistancia.Location = new System.Drawing.Point(12, 54);
+            this.lblDistancia.Name = "lblDistancia";
+            this.lblDistancia.Size = new System.Drawing.Size(93, 17);
+            this.lblDistancia.TabIndex = 1;
+            this.lblDistancia.Text = "Distancia (km):";
+            // 
+            // lblDiasEstadia
+            // 
+            this.lblDiasEstadia.AutoSize = true;
+            this.lblDiasEstadia.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiasEstadia.Location = new System.Drawing.Point(12, 91);
+            this.lblDiasEstadia.Name = "lblDiasEstadia";
+            this.lblDiasEstadia.Size = new System.Drawing.Size(101, 17);
+            this.lblDiasEstadia.TabIndex = 2;
+            this.lblDiasEstadia.Text = "Días de estadía:";
+            // 
+            // txtDistancia
+            // 
+            this.txtDistancia.Location = new System.Drawing.Point(116, 54);
+            this.txtDistancia.Name = "txtDistancia";
+            this.txtDistancia.Size = new System.Drawing.Size(121, 20);
+            this.txtDistancia.TabIndex = 5;
+            this.txtDistancia.TextChanged += new System.EventHandler(this.txtDistancia_TextChanged);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(162, 150);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // numDias
+            // 
+            this.numDias.Location = new System.Drawing.Point(116, 91);
+            this.numDias.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numDias.Name = "numDias";
+            this.numDias.Size = new System.Drawing.Size(120, 20);
+            this.numDias.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::pryManasseroFerro.Properties.Resources.images;
+            this.pictureBox1.Location = new System.Drawing.Point(262, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(135, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmOrganizacionferro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(409, 187);
+            this.Controls.Add(this.numDias);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtDistancia);
+            this.Controls.Add(this.lblDiasEstadia);
+            this.Controls.Add(this.lblDistancia);
+            this.Controls.Add(this.lblBoletos);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmOrganizacionferro";
+            this.Text = "Organización Ferro";
+            this.Load += new System.EventHandler(this.frmOrganizacionferro_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numDias)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBoletos;
+        private System.Windows.Forms.Label lblDistancia;
+        private System.Windows.Forms.Label lblDiasEstadia;
+        private System.Windows.Forms.TextBox txtDistancia;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numDias;
+    }
+}
+
